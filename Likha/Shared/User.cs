f854d679f681911536c5fd3022aec9ec213ea07d
Mark Joseph Batinga <batingamarkjoseph@gmail.com>
2022-03-28 18:40:08 +0800

    Revert "Add Button for registration and dowgrade to .net5"
    This reverts commit e847a1bc25249249b63fcbc587cfe09073d7ddab.

--- Likha/Shared/User.cs
+++ Likha/Shared/User.cs
@@ -11,9 +11,5 @@ namespace Likha.Shared
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
     }
 }
